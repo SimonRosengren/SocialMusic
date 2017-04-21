36bbcef73a8eb232d7677465d1329e9fcc548df4
LuddeW <ludwig.wiktorsson@gmail.com>
2017-04-21 13:20:25 +0200

    Adding a model for albums

---
+++ SocialMusic/SocialMusic/Models/Album.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SocialMusic.Models
+{
+    public class Album
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Artist { get; set; }
+    }
+}
