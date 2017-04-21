c9c671ad85f88b8b8257a5700a0f1ab27c778a23
LuddeW <ludwig.wiktorsson@gmail.com>
2017-04-21 14:03:23 +0200

    Adding an albumcontroller

---
+++ SocialMusic/SocialMusic/Controllers/AlbumController.cs
@@ -0,0 +1,32 @@
+﻿using SocialMusic.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace SocialMusic.Controllers
+{
+    public class AlbumController : ApiController
+    {
+        Album[] albums = new Album[]
+        {
+            new Album {ID = 1, Name = "Coloring Book", Artist = "Chance the rapper"  },
+            new Album {ID = 2, Name = "Starboy", Artist = "The Weeknd" },
+            new Album {ID = 3, Name = "DAMN.", Artist = "Kendrick Lamar" }
+        };
+
+        public IEnumerable<Album> GetAllAlbums()
+        {
+            return albums;
+        }
+
+        public IHttpActionResult GetAlbum(int id)
+        {
+            var album = albums.FirstOrDefault((s) => s.ID == id);
+            if (album == null)
+            {
+                return NotFound();
+            }
+            return Ok(album);
+        }
+    }
+}
