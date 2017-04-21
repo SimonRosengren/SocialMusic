d2461cadbf3ac649dbcc1de85736b4af41c8fe53
xzorcious <simonrosengren123@gmail.com>
2017-04-21 14:18:01 +0200

    Adding a way to see registered members

--- SocialMusic/SocialMusic/Controllers/HomeController.cs
+++ SocialMusic/SocialMusic/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SocialMusic.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,24 @@ namespace SocialMusic.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(User user)
+        {
+            using (var db = new UserDbContext())
+            {
+                user.Created = DateTime.Now;
+                
+
+                db.Users.Add(user);
+                db.SaveChanges();
+            }
+            return Redirect("Index");
         }
     }
 }
