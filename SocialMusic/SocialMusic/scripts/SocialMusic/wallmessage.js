1870a81f276a9c263be673058ed41749d61a21a9
LuddeW <ludwig.wiktorsson@gmail.com>
2017-05-08 16:19:16 +0200

    Adding Wallmessage script to a jsfile instead

---
+++ SocialMusic/SocialMusic/scripts/SocialMusic/wallmessage.js
@@ -0,0 +1,18 @@
+﻿Socialmusic = {
+    postWallmessage: function () {
+        $('#message').submit(function (e) {
+            /*Prevents the page to reaload like it normaly should*/
+            e.preventDefault()
+            /*Do the post through Ajax*/
+            $.ajax({
+                type: 'POST',
+                url: '/wallmessages/PostToWall',
+                data: { message: $('#input').val() }
+            })
+            /*Adds the new message to the html*/
+            $('#newMessages').append('<li>' + $('#input').val() + '</li>')
+            /*Sets the text input to empty*/
+            $('#input').val('')
+        })
+    }
+}
